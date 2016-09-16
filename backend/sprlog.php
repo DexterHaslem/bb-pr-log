d299c5dc8a9edf75c158e088b6e6a8ccad162573
Dexter Haslem <dexter.haslem@gmail.com>
2016-09-15 19:14:04 -0600

    initial commit

---
+++ backend/sprlog.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dexter
+ * Date: 9/15/2016
+ * Time: 7:11 PM
+ */
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+$url = $_REQUEST['url'];
+$ret = '';
+if ($url != '') {
+    $ret = file_get_contents($url);
+}
+echo $ret;
+?>
