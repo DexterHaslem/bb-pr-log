e1aba87bbb425a0da10ed122c27ad24db68eb08e
Dexter Haslem <dexter.haslem@gmail.com>
2016-09-15 21:12:38 -0600

    i hate php stupid shit, backend sorta working for raw logging now

--- backend/sprlog.php
+++ backend/sprlog.php
@@ -10,34 +10,60 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 
-function add_log($type, $payload) {
-    //X-Event-Key: pullrequest:rejected
-    $servername = "localhost";
-    $username = "rq3";
-    $password = "eB6rW4RMeV";
-    $dbname = "rq3_sprlog";
+$servername = "localhost";
+$username = "rq3";
+$password = "eB6rW4RMeV";
+$dbname = "rq3_sprlog";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    //echo "Connected successfully";
+if (!isset($_REQUEST['action'])) {
+    die("no action");
 }
 
 $action = $_REQUEST['action'];
+
 switch ($action) {
-    case 'add':
-        $type = $_SERVER['X-Event-Key'];
-        $payload = file_get_contents('php://input');
-        add_log($type, $payload);
+    case "add":
+        $add_type = $conn->real_escape_string($_SERVER['X-Event-Key']);
+        $add_payload = file_get_contents('php://input');
+        // this is stupid, convert from json, then back to json to strip crap
+        // like newlines etc, makes it nice normalized json in db
+        $add_payload_decoded = json_decode($add_payload, true );
+        $add_payload_converted = json_encode($add_payload_decoded);
+        $stmt = $conn->prepare("INSERT INTO log(type, payload) VALUES (?, ?)");
+        $stmt->bind_param('ss', $add_type, $add_payload_converted);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        //echo $add_payload_converted;
+        return;
+    case "getRecent":
+        $query = "select type,payload,time from log limit 25";
         break;
-    case 'getRecent':
-        break;
-    case 'getAll':
+    case "getAll":
+        $query = "select type,payload,time from log";
         break;
     default:
-        echo json_encode("Invalid action");
-        break;
+        die("Invalid action");
 }
+
+echo "bottom";
+
+$result = $conn->query($query);
+if (!$result) {
+    $message = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+}
+
+while ($row = $result->fetch_assoc()) {
+    $ret[] = $row;//json_encode($row);
+}
+echo json_encode($ret);
+
+$conn->close();
 ?>
