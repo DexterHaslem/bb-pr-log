a6263a19af583911ca1632df79562d1dafa8fb65
Dexter Haslem <dexter.haslem@gmail.com>
2016-09-18 20:10:51 -0600

    dynamic table thingy, dummy data test

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -1,7 +1,7 @@
 /**
  * Created by Dexter on 9/18/2016.
  */
-import {inject} from 'aurelia-framework';
+import {inject, computedFrom} from 'aurelia-framework';
 import {Api} from '../../api';
 import * as R from 'ramda';
 
@@ -9,5 +9,33 @@ import * as R from 'ramda';
 export class Stats {
   constructor(api) {
     this.api = api;
+    this.stats = null;
+  }
+
+  @computedFrom("api")
+  get allStats() {
+    console.log("get allStats");
+    if (this.stats) {
+      return this.stats;
+    }
+
+    this.stats = {};
+    this.stats.columns = [{
+      title: "title1",
+      field: "col1"
+    }, {
+      title: "title2",
+      field: "col2"
+    },
+    {
+      title: "title3",
+      field: "col3"
+    }];
+    this.stats.rows = [{
+      col1: "foo",
+      col2: "bar",
+      col3: "baz",
+    }];
+    return this.stats;
   }
 }
---
+++ src/components/stats/statsTable.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Dexter on 9/18/2016.
+ */
+import {bindable} from "aurelia-framework";
+
+export class StatsTable {
+
+  @bindable allStats;
+
+  @bindable title;
+
+  constructor() {
+    console.log(this);
+  }
+}
